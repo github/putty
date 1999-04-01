0e0fd7702c20587fecec1990df8251e76192f6be
Ben Harris <bjh21@bjh21.me.uk>
1999-04-01 21:25:06 +0000

    New version-number system.  Doesn't yet do the "about" box.
    [originally from svn r148]

---
+++ version.h
@@ -0,0 +1,14 @@
+/* PuTTY version number */
+/*
+ * Note that this file is used by resource compilers that don't
+ * implement the funkier bits of CPP
+ */
+
+#define PUTTY_VERS_MAJOR	0
+#define PUTTY_VERS_MAJOR_BCD	0x00
+#define PUTTY_VERS_MINOR	45
+#define PUTTY_VERS_MINOR_BCD	0x45
+#define PUTTY_VERS_PRERELEASE	0
+#define PUTTY_VERS_STATE	development
+#define PUTTY_VERS_SHORT_STR	"0.45d0"
+#define PUTTY_VERS_LONG_STR	"Dev 0.45 Mac 0"
---
+++ version.r
@@ -0,0 +1,24 @@
+/*
+ * PuTTY version resource
+ * (separated because it gets rebuilt for each architecture)
+ */
+
+#include "version.h"
+#include "Types.r"
+
+#ifndef BUILD_ARCH
+#define BUILD_ARCH "unknown"
+#endif
+
+resource 'vers' (1, purgeable) {
+    PUTTY_VERS_MAJOR_BCD,
+    PUTTY_VERS_MINOR_BCD,
+    PUTTY_VERS_STATE,
+    PUTTY_VERS_PRERELEASE,
+    2,			/* Region code (2 = UK) */
+    PUTTY_VERS_SHORT_STR,
+
+    PUTTY_VERS_LONG_STR " (" BUILD_ARCH ")\n"
+    "Copyright \$a9 Simon Tatham 1997-9",
+};
+
